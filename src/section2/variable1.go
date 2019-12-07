34686b3372279d3cce9888a5744bd45cfacbc2d4
Taekyung2 <ltk3934@daum.net>
2019-12-07 14:58:02 +0900

    first

---
+++ src/section2/variable1.go
@@ -0,0 +1,18 @@
+// 변수 1
+package main
+
+import "fmt"
+
+func main() {
+	//기본 초기화
+	var a int
+	var b string
+	var c, d, e int
+	var f, g, h int = 1, 2, 3
+	var i float32 = 11.4
+	var j string = "Hi! Golang!"
+	var k = 4.74 // 자동 타입 선언 -> auto
+	var l = "Hi Seoul."
+	var m = true
+	
+}
