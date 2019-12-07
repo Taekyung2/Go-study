2ca2a9eed65a614ab03ce3aa2bdca8e9101bde36
Taekyung2 <ltk3934@daum.net>
2019-12-07 17:24:23 +0900

    첫 날

--- src/section2/variable3.go
+++ src/section2/variable3.go
@@ -11,11 +11,11 @@ func main() {
 	shortVar1 := 3
 	shortVar2 := "Test"
 	shortVar3 := false
-	
+
 	//shortVar3 := true // 예외 발생
 	fmt.Println("shortVar1 : ", shortVar1, "shortVar2 : ", shortVar2, "shortVar3 : ", shortVar3)
 	//example
 	if i:= 0; i<11 {
 		fmt.Println("Short Variable Test Success!")
-	}	
+	}
 }
